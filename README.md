fd66a536b3cf1e7e9ea9306bb50d0971b2af5bc0
gurshant kahlon <gkahlon40@rrc.ca>
2025-05-21 09:19:50 -0500

    initial commit :Documented Project.

---
+++ README.md
@@ -0,0 +1,9 @@
+# Module 02 Demonstration 
+
+## Description
+
+Introduction to Python: Documentation, variable, printing
+
+## Author
+
+Gurshant singh
---
+++ test.py
@@ -0,0 +1,6 @@
+"""
+Description: Module 02 demonstration 
+Author: Gurshant singh 
+Date: 21-05-2025
+Usage: To demonstrate content from Module 02 
+"""
