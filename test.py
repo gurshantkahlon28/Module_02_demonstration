ce7c1609815564c199baef4deb5be754d3484f1b
gurshant kahlon <gkahlon40@rrc.ca>
2025-05-23 01:15:21 -0500

    Experimented with math operators and order of operations.

--- test.py
+++ test.py
@@ -4,3 +4,9 @@ Author: Gurshant singh
 Date: 21-05-2025
 Usage: To demonstrate content from Module 02 
 """
+
+absolute_value = abs(-12)
+print ('absolute value:',absolute_value)
+
+# Function embedded within another function.
+print ('absolute value:', abs(-12))
